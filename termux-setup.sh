2f310b7e3d0429e351999783c07e1e62a5688061
jongwony <lastone9182@gmail.com>
2018-08-17 00:20:18 +0900

    android sync update

--- termux-setup.sh
+++ termux-setup.sh
@@ -1,13 +1,23 @@
 # android
+pkg install bc
 pkg install clang
-pkg install git
-pkg install zsh
+pkg install ctags
 pkg install curl
-pkg install vim
-pkg install tmux
-pkg install python
+pkg install diffutils
+pkg install dnsutils
+pkg install gem
+pkg install git
+pkg install man
+pkg install mariadb
+pkg install parallel
 pkg install perl
+pkg install python
+pkg install ruby
+pkg install sqlite
+pkg install sqlite3
 pkg install termux-api
-
+pkg install tmux
+pkg install vim
+pkg install vim-python
+pkg install zsh
 termux-storage-setup
-
